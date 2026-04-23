5c7d7753ddcaae8e86e590fd6e44e0f57c70deb6
Seifeddine <seifeddine.benamor@etudiant-enit.utm.tn>
2026-04-23 23:39:42 +0200

    chore: add lifespan manager

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from app.core.config import settings
+from app.api.v1.router import api_router
+from app.db.session import engine
+from app.db.models import Base
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
 
 def create_app() -> FastAPI:
     app = FastAPI(
         title=settings.PROJECT_NAME,
         version="1.0.0",
-        openapi_url=f"{settings.API_V1_STR}/openapi.json",  # where Swagger UI gets its schema
+        openapi_url=f"{settings.API_V1_STR}/openapi.json",# where Swagger UI gets its schema 
+        lifespan=lifespan,
     )
 
     # Allow the frontend dev server to call the API
@@ -18,6 +31,6 @@ def create_app() -> FastAPI:
         allow_headers=["*"],
     )
 
-    # TODO: include API router here once created
+    app.include_router(api_router, prefix=settings.API_V1_STR)
 
     return app
