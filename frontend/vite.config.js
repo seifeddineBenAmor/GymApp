d78a82aea96253661cb5d0a69916b9a0343d2726
Seifeddine <seifeddine.benamor@etudiant-enit.utm.tn>
2026-04-23 23:43:59 +0200

    chore: add frontend basics

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,        // needed for Docker
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://api:8000",   // docker-compose service name
-        changeOrigin: true,
-      },
-    },
-  },
-});
+})
