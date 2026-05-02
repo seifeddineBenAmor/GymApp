382e70d214db3b6c0524de83fa1340aed4e43429
Seifeddine <seifeddine.benamor@etudiant-enit.utm.tn>
2026-05-02 18:12:05 +0200

    chore : register cantegory router in router

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import auth
+from app.api.v1.endpoints import categories
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
+api_router.include_router(categories.router, prefix="/categories", tags=["Categories"])
